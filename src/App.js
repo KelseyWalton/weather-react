60ce9469626dea5ffaf278c85b8b720a52b356c2
KelseyWalton <Kelsey.walton6066@gmail.com>
2023-01-26 13:34:35 -0600

    added link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <WeatherSearch />
+      <p className="footer">
+        Made by Kelsey Walton Check the{" "}
+        <a href="https://github.com/KelseyWalton/weather-react">
+          open-source code
+        </a>
+        .
+      </p>
     </div>
   );
 }
