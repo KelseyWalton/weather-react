d15b1e24a42ca9a41b28afbf6d3b8e2aa0a4b98e
KelseyWalton <Kelsey.walton6066@gmail.com>
2023-01-29 20:35:27 -0600

    created current weather

---
+++ src/CurrentWeather.js
@@ -0,0 +1,36 @@
+import React from "react";
+import CurrentDate from "./CurrentDate";
+import UnitConversion from "./UnitConversion";
+import "./CurrentWeather.css";
+
+export default function WeatherResults(props) {
+  return (
+    <div className="WeatherResults">
+      <div className="row current-stats">
+        <div className="col-1 icon">
+          <img
+            src={props.data.iconUrl}
+            alt="weather icon"
+            className="current-icon"
+          />
+        </div>
+        <ul className="col-6 current-weather">
+          <UnitConversion
+            celsius={props.data.temperature}
+            kilometers={props.data.wind}
+            humidity={props.data.humidity}
+          />
+        </ul>
+        <ul className="col-5 location">
+          <li className="current-city">{props.data.city}</li>
+          <li className="day-time">
+            <CurrentDate date={props.data.date} />
+          </li>
+          <li className="current-weather-desc text-capitalize">
+            {props.data.description}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
